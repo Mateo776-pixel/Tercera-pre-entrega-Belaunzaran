39bbcceb208646d30db86bb8273696894b4838cd
Mateo <Mateopython@gmail.com>
2024-11-26 20:30:10 -0300

    Cambie algunos valores

--- AppV/urls.py
+++ AppV/urls.py
@@ -4,8 +4,8 @@ from AppV import views
 
 urlpatterns = [
     path('inicio/', views.inicio, name='inicio'),
-    path('cli/', views.clientes, name='clientes'),
-    path('prov/', views.proveedores, name='proveedores'),
+    path('clientes/', views.clientes, name='clientes'),
+    path('proveedores/', views.proveedores, name='proveedores'),
     path('vendedores/', views.vendedores, name='vendedores'), 
     path('expresos/', views.expresos, name='expresos'),  
     path('buscar_cliente/', views.buscar_cliente, name='buscar_cliente'),
